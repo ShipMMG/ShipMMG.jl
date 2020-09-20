bfe9474a4a5112c314c9f829f62ad1a85375534d
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2020-09-20 12:41:04 +0900

    separete kt.jl test code from runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,19 @@ using Test
 
 @testset "ShipMMG.jl" begin
 
-    @testset "kt.jl" begin
-        K_log = 0.155  # [1/s]
-        T_log = 80.5  # [s]
-        u0 = 20 * (1852.0 / 3600)  # [m/s] (knot * 1852/3600)
-        duration = 500  # [s]
-        sampling = 10000
-        time_list = range(0.0,stop=duration,length=sampling)
-        Ts = 50.0
-        δ_list = 10.0 * pi / 180.0  * sin.(2.0 * pi / Ts * time_list) # [rad]
-        @time sol=kt_simulate(time_list, δ_list, K_log, T_log, u0)
+    include("test_kt.jl")
+
+    @testset "rotate_pos func" begin
+        @test rotate_pos([4,0], deg2rad(90)) ≈ [0,4]
+        @test rotate_pos([4,0], deg2rad(-90)) ≈ [0,-4]
     end
+
+    @testset "square func" begin
+        @test square(1, 0, [4,2], deg2rad(90)) ≈ [[0,0,2,2],[2,-2,-2,2]]
+    end
+
+    # @testset "draw_gif_result func" begin
+    #     time, x, y, ψ, u, r, δ = kt_results
+    # end
 
 end
---
+++ test/test_kt.jl
@@ -0,0 +1,11 @@
+@testset "kt.jl" begin
+    K_log = 0.155  # [1/s]
+    T_log = 80.5  # [s]
+    u0 = 20 * (1852.0 / 3600)  # [m/s] (knot * 1852/3600)
+    duration = 500  # [s]
+    sampling = 10000
+    time_list = range(0.0,stop=duration,length=sampling)
+    Ts = 50.0
+    δ_list = 10.0 * pi / 180.0  * sin.(2.0 * pi / Ts * time_list) # [rad]
+    @time kt_results=kt_simulate(time_list, δ_list, K_log, T_log, u0)
+end
