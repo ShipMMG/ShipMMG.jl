e3fc3133b49a1e35304c158d7639729a95da4262
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2020-09-20 12:39:56 +0900

    add draw gif func

--- src/ShipMMG.jl
+++ src/ShipMMG.jl
@@ -5,8 +5,36 @@ module ShipMMG
     using Dierckx
     using LinearAlgebra
     using Sundials
+    using Plots
+    using LaTeXStrings
 
     include("kt.jl")
-    export kt_simulate
+    export kt_simulate, draw_gif_result, rotate_pos, square
+
+    function draw_gif_result(time, x, y, ψ, file_path, fps=10)
+        anim = @animate for i in 1:length(time)
+            plot(x, y, label="", xlabel=L"x \textrm{[m]}", ylabel=L"y \textrm{[m]}", linestyle = :dot, aspect_ratio=:equal)
+            ship = square(x[i], y[i], [20,5], ψ[i])
+            plot!(Shape(ship[1], ship[2]), label="")
+            scatter!([x[i]], [y[i]], seriestype = :scatter, title="time = $(time[i])", label="")
+        end
+        gif(anim, file_path, fps=fps)
+    end
+
+    function rotate_pos(pos, angle)label=""
+        rotate_matrix = [cos(angle) -sin(angle) ; sin(angle) cos(angle)]
+        rotate_matrix * pos
+    end
+
+    function square(center_x, center_y, shape, angle)
+        square_xy = [
+            rotate_pos([shape[1], shape[2]]/2,angle)+[center_x,center_y],
+            rotate_pos([-shape[1], shape[2]]/2,angle)+[center_x,center_y],
+            rotate_pos([-shape[1], -shape[2]]/2,angle)+[center_x,center_y],
+            rotate_pos([shape[1], -shape[2]]/2,angle)+[center_x,center_y],
+        ]
+        xy = hcat(square_xy...)
+        [xy[1,:], xy[2,:]]
+    end
     
 end
