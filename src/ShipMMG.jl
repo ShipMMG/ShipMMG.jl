1643a6c56a9678e48ee3796dc983940914058ee2
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-09-30 18:30:05 +0900

    arrange draw func by creating draw.jl

--- src/ShipMMG.jl
+++ src/ShipMMG.jl
@@ -8,52 +8,13 @@ using Sundials
 using Plots
 using LaTeXStrings
 
-export draw_gif_result, rotate_pos, square
-
 include("kt.jl")
 export kt_simulate
 
 include("mmg.jl")
 export Mmg3DofBasicParams, Mmg3DofManeuveringParams, mmg_3dof_simulate
 
-function draw_gif_result(time, x, y, ψ, shape, file_path, fps = 10)
-    anim = @animate for i = 1:length(time)
-        plot(
-            x,
-            y,
-            label = "",
-            xlabel = L"x \textrm{[m]}",
-            ylabel = L"y \textrm{[m]}",
-            linestyle = :dot,
-            aspect_ratio = :equal,
-        )
-        ship = square(x[i], y[i], shape, ψ[i])
-        plot!(Shape(ship[1], ship[2]), label = "")
-        scatter!(
-            [x[i]],
-            [y[i]],
-            seriestype = :scatter,
-            title = "time = $(time[i])",
-            label = "",
-        )
-    end
-    gif(anim, file_path, fps = fps)
-end
-
-function rotate_pos(pos, angle)
-    rotate_matrix = [cos(angle) -sin(angle); sin(angle) cos(angle)]
-    rotate_matrix * pos
-end
-
-function square(center_x, center_y, shape, angle)
-    square_xy = [
-        rotate_pos([shape[1], shape[2]] / 2, angle) + [center_x, center_y],
-        rotate_pos([-shape[1], shape[2]] / 2, angle) + [center_x, center_y],
-        rotate_pos([-shape[1], -shape[2]] / 2, angle) + [center_x, center_y],
-        rotate_pos([shape[1], -shape[2]] / 2, angle) + [center_x, center_y],
-    ]
-    xy = hcat(square_xy...)
-    [xy[1, :], xy[2, :]]
-end
+include("draw.jl")
+export draw_gif_result, rotate_pos, square
 
 end
