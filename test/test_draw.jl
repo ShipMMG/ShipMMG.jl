4256d5bd89726cb65d0412f8c8aa0a604cda8855
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-10-01 19:25:08 +0900

    add calc_position func

--- test/test_draw.jl
+++ test/test_draw.jl
@@ -1,3 +1,14 @@
+@testset "calc_position func" begin
+    time_vec = [0.0, 2.0]
+    u_vec = [1.0, 1.0]
+    v_vec = [0.0, 0.0]
+    r_vec = [0.0, 0.0]
+    x, y, ψ = calc_position(time_vec, u_vec, v_vec, r_vec, x0 = 1.0, y0 = 1.0, ψ0 = 0.0)
+    @test x[2] ≈ 3.0
+    @test y[2] ≈ 1.0
+    @test ψ[2] ≈ 0.0
+end
+
 @testset "rotate_pos func" begin
     @test rotate_pos([4, 0], deg2rad(90)) ≈ [0, 4]
     @test rotate_pos([4, 0], deg2rad(-90)) ≈ [0, -4]
