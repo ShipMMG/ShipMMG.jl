47ec7b462017aee81b37588f25c9b552bd9815df
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2021-10-16 18:29:43 +0900

    hide rotate_pos and square func

--- test/test_draw.jl
+++ test/test_draw.jl
@@ -1,12 +1,3 @@
-@testset "rotate_pos func" begin
-    @test rotate_pos([4, 0], deg2rad(90)) ≈ [0, 4]
-    @test rotate_pos([4, 0], deg2rad(-90)) ≈ [0, -4]
-end
-
-@testset "square func" begin
-    @test square(1, 0, [4, 2], deg2rad(90)) ≈ [[0, 0, 2, 2], [2, -2, -2, 2]]
-end
-
 @testset "draw_gif_result func" begin
     @testset "using KT" begin
         K_log = 0.155  # [1/s]
