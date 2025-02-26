ec932af58c770d17231d6a86136722e7d3fb4a47
gomi-satoru <gomi-satoru-rv@ynu.jp>
2025-02-26 10:32:47 +0900

    changed type of p from array(vector) to tuple

--- src/mmg.jl
+++ src/mmg.jl
@@ -1103,7 +1103,7 @@ function create_model_for_mcmc_sample_mmg(
     N_vrr_dash_start = 0.055
     N_rrr_dash_start = -0.013
 
-    p = [
+    p = (
         R_0_dash_start,
         X_vv_dash_start,
         X_vr_dash_start,
@@ -1121,7 +1121,7 @@ function create_model_for_mcmc_sample_mmg(
         N_vvr_dash_start,
         N_vrr_dash_start,
         N_rrr_dash_start,
-    ]
+    )
 
     u0 = 2.29 * 0.512
     v0 = 0.0
@@ -1152,7 +1152,7 @@ function create_model_for_mcmc_sample_mmg(
         N_vrr_dash ~ N_vrr_dash_prior_dist
         N_rrr_dash ~ N_rrr_dash_prior_dist
 
-        p = [
+        p = (
             R_0_dash,
             X_vv_dash,
             X_vr_dash,
@@ -1170,7 +1170,7 @@ function create_model_for_mcmc_sample_mmg(
             N_vvr_dash,
             N_vrr_dash,
             N_rrr_dash,
-        ]
+        )
         prob = remake(prob1, p=p)
         sol = solve(prob, solver, abstol=abstol, reltol=reltol)
         predicted = sol(time_obs)
@@ -1391,7 +1391,7 @@ function create_model_for_mcmc_sample_mmg(
     N_vrr_dash_start = 0.055
     N_rrr_dash_start = -0.013
 
-    p = [
+    p = (
         R_0_dash_start,
         X_vv_dash_start,
         X_vr_dash_start,
@@ -1409,7 +1409,7 @@ function create_model_for_mcmc_sample_mmg(
         N_vvr_dash_start,
         N_vrr_dash_start,
         N_rrr_dash_start,
-    ]
+    )
 
     u0 = 2.29 * 0.512
     v0 = 0.0
